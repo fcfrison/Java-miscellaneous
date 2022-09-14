fa428e9a28bac77c96b83db3299db1491b8e764e
FELIPE FRISON <fcfrison@gmail.com>
2022-09-14 19:34:01 -0300

    Small modifications in 'src'.

--- SoftwareEng/heranca_exercicios/Exercicio_2/src/Economy.java
+++ SoftwareEng/heranca_exercicios/Exercicio_2/src/Economy.java
@@ -7,22 +7,7 @@ public class Economy extends Passagem{
         super(cpf,nome,assento,qtde,pesos);
     }
     protected  Double custoBagagens(Integer qtde, ArrayList<Double> pesos){
-        if(pesos.size()!=qtde){
-            throw new IllegalArgumentException("A quantidade de pesos de" +
-                                    "bagagens informadas é diferente da " +
-                                    "quantidade informada"); 
-        }
-        if(qtde<0){
-            throw new IllegalArgumentException("A quantidade informada deve"+
-                                                " ser um valor positivo");
-        }
-        Double acumulador=0.0;
-        for (Double peso : pesos) {
-            if(peso<0){
-                throw new IllegalArgumentException("Nao sao aceitos pesos negativos.");
-            }
-            acumulador+=peso;
-        }
+        Double acumulador=super.custoBagagens(qtde, pesos);
         Double valorExtra = qtde*10.00;
         return acumulador*.5 + valorExtra;
     }
--- SoftwareEng/heranca_exercicios/Exercicio_2/src/Executive.java
+++ SoftwareEng/heranca_exercicios/Exercicio_2/src/Executive.java
@@ -9,11 +9,6 @@ public class Executive extends Passagem {
     @Override
     protected Double custoBagagens(Integer qtde, ArrayList<Double> pesos){
         Double acumulador=0.0;
-        if(pesos.size()!=qtde){
-            throw new IllegalArgumentException("A quantidade de pesos de" +
-                                    "bagagens informadas é diferente da " +
-                                    "quantidade informada"); 
-        }
         if(qtde<0){
             throw new IllegalArgumentException("A quantidade informada deve"+
                                                 " ser um valor positivo");
@@ -21,13 +16,8 @@ public class Executive extends Passagem {
         if(qtde<=2){//duas bagagens são isentas de custo.
             return 0.00;
         }
-        for (Double peso : pesos) {
-            if(peso<0){
-                throw new IllegalArgumentException("Nao sao aceitos pesos negativos.");
-            }
-            acumulador+=peso;
-        }
-        return acumulador*.5;
+        acumulador = super.custoBagagens(qtde, pesos);
+        return acumulador;
     }
 
     Long getMilhas(){
